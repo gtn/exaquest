85dc4e96ffac649c814dc5be39802c3b5d79527d
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2023-04-12 22:11:47 +0200

    multiselect fix

--- classes/form/autofill_helper_form.php
+++ classes/form/autofill_helper_form.php
@@ -14,14 +14,14 @@ class autofill_helper_form extends \moodleform {
 
     }
 
-    public function create_autocomplete_html($autocompleteoptions, $questionbankentryid) {
+    public function create_autocomplete_html($autocompleteoptions, $id=0) {
         $options = array(
             'multiple' => true,
         );
         // moodle/lib/form/autocomplete.php
         // if I keep 'selectedusers' for every mfrom it breaks. Only the first will be displayed correctly, the rest will show a simple select with options, without the added fields.
         // ==> needs to be unique
-        $element = $this->_form->addElement('autocomplete', 'selectedusers'.$questionbankentryid, 'Fragenersteller', $autocompleteoptions, $options);
+        $element = $this->_form->addElement('autocomplete', 'selectedusers'.$id, 'Fragenersteller', $autocompleteoptions, $options);
         return $element->toHtml();
         // how to use example: enrol_users_form has this code:
     }
