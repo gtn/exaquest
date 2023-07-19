f91132d06118aa665155cf3cf55c1a6507ea3662
fabiopernegger <fabio.pernegger@gmail.com>
2023-07-19 10:21:57 +0200

    added empty selection so no user is preselected in dropdown

--- classes/form/autofill_helper_form.php
+++ classes/form/autofill_helper_form.php
@@ -33,6 +33,7 @@ class autofill_helper_form extends \moodleform {
         // moodle/lib/form/autocomplete.php
         // if I keep 'selectedusers' for every mfrom it breaks. Only the first will be displayed correctly, the rest will show a simple select with options, without the added fields.
         // ==> needs to be unique
+        array_unshift($autocompleteoptions, "");
         $element = $this->_form->addElement('autocomplete', 'selectedusers'.$id, 'Fragenersteller', $autocompleteoptions, $options);
         return $element->toHtml();
         // how to use example: enrol_users_form has this code:
