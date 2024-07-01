2ec135dcad7da832c841a3298c78643ea4def53a
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2024-07-01 10:21:22 +0200

    fix typo version

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exaquest';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version = 2024062701;
+$plugin->version = 2024070100;
 $plugin->requires = 2010112400;
 $plugin->dependencies = [
     'local_table_sql' => 2024052800,
